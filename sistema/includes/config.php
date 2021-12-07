9cbdccdb936149a4b70db2a70650d41f649088e2
Eric Faustino Alvim <73504394+EricFaustino@users.noreply.github.com>
2021-12-06 22:14:53 -0300

    Update config.php

--- sistema/includes/config.php
+++ sistema/includes/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'sql308.epizy.com';
-$user = 'epiz_30533349';
-$db = 'epiz_30533349_digital_life';
-$pass = 'zvJpyyqZApF2';
+$host = 'us-cdbr-east-04.cleardb.com';
+$user = 'bc9b379ca099b3';
+$db = 'heroku_395e6c8cb2485c9';
+$pass = '58f5ec34';
 
     try {
         $pdo = new PDO('mysql:host='.$host.';dbname='.$db.';charset=utf8',$user,$pass);
